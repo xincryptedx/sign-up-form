2199e9b0136489c1bea3b8be3518d5f57fa035c6
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-02-23 19:11:35 -0500

    Add function to match passwords
    This function compares the values of the password and confirm password
    inputs and if they are not a match it will inform the user and stop
    the form from submitting.

--- signup-form.js
+++ signup-form.js
@@ -1,6 +1,20 @@
-const pwd = document.getElementById('pwd');
-const pwdConfirm = document.getElementById('pwd-confirm');
+var pwd = document.getElementById('pwd');
+var pwdConfirm = document.getElementById('pwd-confirm');
+var submitBtn = document.getElementById('submit-btn');
 
-function matchPwd(){
-    console.log("Hello world!");
+submitBtn.addEventListener('click', function(e){
+    matchPwd(e)});
+
+function matchPwd(e){
+    console.log(pwd.value + " " + pwdConfirm.value);
+    if (pwd.value === pwdConfirm.value) {
+        console.log("Values do match!");
+        pwdConfirm.setCustomValidity('');
+    }
+    else {
+        pwdConfirm.setCustomValidity('Passwords do not match.');
+        pwdConfirm.reportValidity();
+        e.preventDefault();
+        console.log("Values do not match!");
+    }
 }
