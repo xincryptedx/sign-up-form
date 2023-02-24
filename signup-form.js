e7fb19d464f575358d8b8c523b81f920c4204928
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-02-23 19:49:52 -0500

    Add red highlight around inputs that are not valid
    This is done by adding a class to the elements during the blur event
    or when the submit button is clicked. If the element is not valid it
    will have the error class added to it, or removed if it is valid.

--- signup-form.js
+++ signup-form.js
@@ -1,20 +1,36 @@
 var pwd = document.getElementById('pwd');
 var pwdConfirm = document.getElementById('pwd-confirm');
 var submitBtn = document.getElementById('submit-btn');
+const inputs = document.querySelectorAll("input");
 
 submitBtn.addEventListener('click', function(e){
     matchPwd(e)});
 
+inputs.forEach(input => {
+    input.addEventListener(
+        "invalid",
+        () => {
+            input.classList.add("error");
+        },
+        false
+    );
+
+    input.addEventListener('blur', () => {
+        if (input.checkValidity()){
+            input.classList.remove("error");
+        }
+    });
+});
+
+
 function matchPwd(e){
     console.log(pwd.value + " " + pwdConfirm.value);
     if (pwd.value === pwdConfirm.value) {
-        console.log("Values do match!");
         pwdConfirm.setCustomValidity('');
     }
     else {
         pwdConfirm.setCustomValidity('Passwords do not match.');
         pwdConfirm.reportValidity();
         e.preventDefault();
-        console.log("Values do not match!");
     }
 }
